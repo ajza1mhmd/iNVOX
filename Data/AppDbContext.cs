efed7a03b4ba61c4fb8662859563e0d44a557a76
Ajsal <Ajsal@EXPSHARJAHSRV>
2025-07-19 10:59:04 +0400

    installed packages & setup files configured

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Invox.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invox.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Users> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("users");
+            modelBuilder.Entity<Users>().HasKey(u => u.Pu_Id);
+        }
+    }
+}
---
+++ Models/DbConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invox.Models
+{
+    public class DbConfig
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string GetConnectionString()
+        {
+            return $"server={Server};port={Port};database={Database};user={Username};password={Password};";
+        }
+    }
+}
